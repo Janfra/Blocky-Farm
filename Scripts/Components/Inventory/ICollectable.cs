4848d54101b6e887525942ec43b270ca766a9132
Jan <93995100+Janfra@users.noreply.github.com>
2022-10-25 16:51:23 +0100

    Ups, dragged and dropped in the wrong place

--- Scripts/Components/Inventory/ICollectable.cs
+++
@@ -1,9 +0,0 @@
-public interface ICollectable
-{
-    /// <summary>
-    /// Collection handling of object
-    /// </summary>
-    /// <param name="inventory">Inventory picking up the object</param>
-    void Collected(InventorySystem inventory);
-    // Default: gameObject.SetActive(!inventory.AddItem(itemData)); 
-}
